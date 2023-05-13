ee94db12dc6806c9354b43883f6b86b6aae16ca5
domothebomo <dfanaris@gmail.com>
2023-05-12 22:34:03 -0700

    added some assets for the player and tilesprite

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ let config = {
     type: Phaser.CANVAS,
     width: 960,
     height: 640,
+    fps: {
+        target: 60,
+        forceSetTimeOut: true
+    },
     scene: [ Title, Play ]
 }
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,20 +4,29 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', './assets/starfield.png');
+        this.load.image('highway', './assets/highway.png');
+        this.load.image('skyline', './assets/skyline.png');
+        this.load.image('player', './assets/cyclist2.png');
     }
   
     create() {
         //console.log('bruh');
         //this.scene.start('playScene');
         //this.text = this.add.text(game.config.width / 2, game.config.height / 2, 'PLAY', {});
-        this.background = this.add.tileSprite(0, 0, 960, 640, 'background').setOrigin(0,0);
+        this.background = this.add.tileSprite(0, 0, 960, 640, 'skyline').setOrigin(0,0);
+        this.highway = this.add.tileSprite(0, 0, 960, 640, 'highway').setOrigin(0,0);
+
         //this.text = this.add.text(game.config.width / 2, game.config.height / 2, 'PLAY', {});
+
+        this.player = this.add.sprite(50, 340, 'player');
     }
 
     update() {
-        this.background.tilePositionX += 600 / game.loop.actualFps;
-        //console.log(game.loop.actualFps);
+        //this.highway.tilePositionX += 600 / game.loop.actualFps;
+        //this.background.tilePositionX += 240 / game.loop.actualFps;
+        this.highway.tilePositionX += 10;
+        this.background.tilePositionX += 1;
+        console.log(game.loop.actualFps);
     }
   
   }
